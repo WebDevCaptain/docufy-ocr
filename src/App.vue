0df376504e20fb2880b71b0927695ac2695e03b5
Shreyash <hello@webdevcaptain.com>
2025-02-03 23:07:54 +0530

    Initialize a vue app with vuetify

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <Docufy />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup></script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-footer height="40" app>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} —
+      <strong>Docufy-OCR: &nbsp;WebDevCaptain</strong>
+    </v-col>
+  </v-footer>
+</template>
